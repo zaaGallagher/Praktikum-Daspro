f0eef1148e69d15e45aab9d219991163129dfd03
zaaGallagher <mrezafahlevi62@gmail.com>
2025-11-12 11:20:53 +0700

    percobaan 1

---
+++ PRAKT 9/ArrayRataNilai17.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        int[] nilaiMhs = new int [10];
+        double total = 0;
+        double rata2;
+
+        for (int i = 0; i < nilaiMhs.length; i++){
+            System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i <  nilaiMhs.length; i++){
+            total += nilaiMhs [i];
+        }
+        rata2 = total/nilaiMhs.length;
+        System.out.println("Rata - rata nilai = " + rata2);
+}
+}
